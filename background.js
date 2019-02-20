7af95788c04842ead6b28a805e84ebcdd9ea229d
Don Marti <dmarti@zgp.org>
2019-02-20 09:46:01 -0800

    WIP Background script to intercept requests

---
+++ background.js
@@ -0,0 +1,46 @@
+var docid = null;
+var happy = true;
+
+function hasCompromisedOrigin(req) {
+	var u = req.originUrl;
+	if (u.indexOf('https://www.youtube.com/') == 0) {
+		return true;
+	}
+}
+
+function isWatchPage(req) {
+	return req.url.indexOf('https://www.youtube.com/watch') == 0;
+}
+
+function checkURL(requestDetails) {
+	if (isWatchPage(requestDetails) && !hasCompromisedOrigin(requestDetails)) {
+		console.log("New, apparently clean, watch page");
+		happy = true;
+	}
+	if (!happy) {
+		console.log("Canceling request for wrong docid.");
+		return({'cancel': true});
+	}
+	if (requestDetails.url.indexOf('&') == -1) {
+		return
+	}
+	var q = requestDetails.url.split('?')[1].split('&');
+	for (var i = 0; i < q.length; i++) {
+		var item = q[i].split('=');
+		if (item[0] == 'docid') {
+			if (!docid) {
+				docid = item[1];
+				return;
+			}
+			if (docid != item[1]) {
+				happy = false;
+			}
+		}
+	}
+}
+
+browser.webRequest.onBeforeRequest.addListener(
+	checkURL,
+	{urls: ["*://www.youtube.com/*"]},
+	["blocking"]
+);
